ca9de52db2205033da53211a2b0f64203dac3406
Matus Zilinec <zilinec.m@gmail.com>
2019-01-16 23:20:29 +0100

    Support n-grams in embedding extractor.

--- botshot_nlu/keywords/embedding.py
+++ botshot_nlu/keywords/embedding.py
@@ -32,29 +32,50 @@ class EmbeddingKeywordExtractor(KeywordExtractor):
                             data_tuples.append((expression, label, entity))
         return data_tuples
 
-    def update(self, data):  # TODO: support multiple words
-        self.keywords = []
+    def update(self, data):
+        self.keywords = {}  # dict of n-grams: [example tuple]
+        self.max_grams = 0
         for expression, label, entity in self._reformat(data):
-            if ' ' in expression: continue
-            vector = self.embedding.get_vector(expression)
-            self.keywords.append((vector, label, entity))
+            tokens = self.tokenizer.tokenize(expression)
+            vector = self._get_centroid(tokens)
+            n_grams = len(tokens)
+            self.max_grams = max(n_grams, self.max_grams)
+            self.keywords.setdefault(n_grams, []).append((vector, label, entity))
 
-    def predict(self, text: str):
-        tokens = self.tokenizer.tokenize(text)
-        text_vectors = []
-        output = {}
+    def _get_centroid(self, tokens: list):
+        # TODO: support other methods than centroid: concat, doc2vec
+        v = np.zeros([300])
         for token in tokens:
-            v = self.embedding.get_vector(token)
-            text_vectors.append(v)
+            v += self.embedding.get_vector(token)
+        return v / len(tokens)
 
-        for i, vector in enumerate(text_vectors):
+    def _predict_ngrams(self, tokens: list, text_vectors: list, keywords, n):
+        output = {}
+        # generate a centroid for each n-gram in text
+        ngram_vectors = [np.mean(text_vectors[i:i+n], axis=0) for i in range(len(text_vectors) - n + 1)]
+        # for each n-gram, find the most similar vector in training examples
+        for i, vector in enumerate(ngram_vectors):
             most_similar_label, most_similar_entity, max_similarity = None, None, -1.0
-            for expression, label, entity in self.keywords:
+            for expression, label, entity in keywords:
                 similarity = 1 - cosine(expression, vector)
                 if similarity > max_similarity:
                     max_similarity = similarity
                     most_similar_label = label
                     most_similar_entity = entity
             if max_similarity >= self.threshold:
-                output.setdefault(most_similar_entity, []).append({"value": tokens[i], "similar_to": most_similar_label, "confidence": max_similarity})
+                matching_text = ' '.join(tokens[i:i+n])
+                output.setdefault(most_similar_entity, []).append({"value": matching_text, "similar_to": most_similar_label, "confidence": max_similarity})
+        return output
+
+    def predict(self, text: str):
+        tokens = self.tokenizer.tokenize(text)
+        text_vectors = []
+        output = {}
+        # embed all tokens in text
+        for token in tokens:
+            v = self.embedding.get_vector(token)
+            text_vectors.append(v)
+        # for each n, compare all n-token substrings with all examples of length n
+        for n, keywords in self.keywords.items():
+            output.update(self._predict_ngrams(tokens, text_vectors, keywords, n))
         return output
